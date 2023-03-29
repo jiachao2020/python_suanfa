327285b7357e231057aa685d6b19be614873c07e
jiachao <jiachao_soochow@163.com>
2023-03-29 13:49:32 +0800

    第二次测试Git版本控制

--- testspark.py
+++ testspark.py
@@ -1,3 +1,5 @@
+# import findspark
+# findspark.init('/path/to/spark/home')
 from pyspark import SparkConf,SparkContext
 conf = SparkConf().setAppName('test')
 try:
@@ -11,4 +13,4 @@ data = ["hello", "world", "hello", "world"]
 rdd = sc.parallelize(data)
 res_rdd = rdd.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-res_rdd.first()
+print(res_rdd.first())
