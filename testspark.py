98162c8fcb4d80ae18d65ef60a399d7e4fa9ce3e
jiachao <jiachao_soochow@163.com>
2023-03-29 15:23:59 +0800

    改写成批量配置的sparksession入口

--- testspark.py
+++ testspark.py
@@ -1,10 +1,23 @@
 from pyspark.sql import SparkSession
-spark=SparkSession.builder.master("local[*]").appName("test").getOrCreate()
+from pyspark import SparkConf
+
+conf=SparkConf()
+#spark.conf.set("spark.executor.memory","5g")
+config=(("spark.app.name","test"),
+        ("spark.executor.memory","6g"),
+        ("spark.master","local[*]")
+)
+#spark=SparkSession.builder.master("local[*]").appName("test").enableHiveSupport().getOrCreate()
+conf.setAll(config)
+spark=SparkSession.builder.config(conf=conf).getOrCreate()
+sc=spark.sparkContext
+
+
 
 data = ["hello", "world", "hello", "world"]
 
-rdd = spark.sparkContext.parallelize(data)
+rdd = sc.parallelize(data,8)
 res_rdd = rdd.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
 print(res_rdd.first())
-spark.stop()
+sc.stop()
