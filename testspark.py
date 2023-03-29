c641671e6d153cd0d2a5663e41cb79bb4a15cebd
jiachao <jiachao_soochow@163.com>
2023-03-29 14:28:47 +0800

    改写成sparksession入口

--- testspark.py
+++ testspark.py
@@ -1,8 +1,4 @@
-# import findspark
-# findspark.init('/path/to/spark/home')
-#from pyspark import SparkConf,SparkContext
 from pyspark.sql import SparkSession
-#conf = SparkConf().setAppName('test').setMaster("local[*]")
 spark=SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 
 data = ["hello", "world", "hello", "world"]
