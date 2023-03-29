9a16748e4978a41ca0ad61b98b119f78c930251d
jiachao <jiachao_soochow@163.com>
2023-03-29 14:27:33 +0800

    第二次测试Git版本控制

--- testspark.py
+++ testspark.py
@@ -1,16 +1,14 @@
 # import findspark
 # findspark.init('/path/to/spark/home')
-from pyspark import SparkConf,SparkContext
-conf = SparkConf().setAppName('test')
-try:
-    sc.stop()
-except:
-    pass
-sc = SparkContext(conf = conf)
+#from pyspark import SparkConf,SparkContext
+from pyspark.sql import SparkSession
+#conf = SparkConf().setAppName('test').setMaster("local[*]")
+spark=SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 
 data = ["hello", "world", "hello", "world"]
 
-rdd = sc.parallelize(data)
+rdd = spark.sparkContext.parallelize(data)
 res_rdd = rdd.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
 print(res_rdd.first())
+spark.stop()
